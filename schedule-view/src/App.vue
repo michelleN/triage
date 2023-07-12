ec706b2c8e427f57a14ffdbd1e300f0f03257184
Michelle Dhanani <michelle@fermyon.com>
2023-07-12 18:55:53 -0400

    add example login button
    + google should be github
    + updates gitignore

--- schedule-view/src/App.vue
+++ schedule-view/src/App.vue
@@ -1,11 +1,92 @@
 <script setup>
 import Calendar from './components/Calendar.vue'
+import GitHubLogo from '../public/favicon.png';
+import { getGitHubUrl } from './utils/getGitHubUrl';
+
+const from = '/';
+
 </script>
 
 <template>
-  <h1> Triage Schedule </h1>
-<Calendar />
+  <div>
+    <h1> Triage Schedule </h1>
+      <Calendar />
+  </div>
+
+  <div class="container">
+    <div class="social-auth">
+      <!-- GitHub OAuth -->
+      <a :href="getGitHubUrl(from)" class="auth-btn github-auth">
+        <img :src="GitHubLogo" alt="GitHub Logo" />
+        <span>Google</span>
+      </a>
+    </div>
+  </div>
 </template>
 
+
 <style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  font-family: Roboto, sans-serif;
+  color: #222;
+  font-size: 1.6rem;
+}
+
+.container {
+  background-color: #2363eb;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.social-auth {
+  max-width: 27rem;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+.auth-btn {
+  background-color: #fff;
+  border-radius: 5px;
+  padding: 0.6rem 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.3s ease-in-out;
+}
+
+.auth-btn img {
+  height: 4rem;
+  margin-right: 1rem;
+}
+.auth-btn span {
+  font-size: 1.8rem;
+}
+
+.auth-btn:hover {
+  box-shadow: 0 1px 13px 0 rgb(0 0 0 / 15%);
+}
+
+.auth-btn.github-auth {
+  margin-bottom: 1.5rem;
+}
 </style>
